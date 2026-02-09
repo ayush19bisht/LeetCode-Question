30da0909019794371be11e847b78a5fd56a17c9b
ayush19bisht <ayushbisht415@gmail.com>
2026-02-09 20:34:09 +0530

    Time: 43 ms (62.02%) | Memory: 54.7 MB (64.68%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-bool isPalin(string s){
+bool isValid(string s){
     string s2 = s;
     reverse(s2.begin() , s2.end());
-    return s2 == s;
+    return s==s2;
 }
 void getParts(string s , vector<string>& partition , vector<vector<string>>& ans){
     if(s.size() == 0){
         ans.push_back(partition);
         return;
     }
-    for(int i=0 ;i<s.size() ; i++){
-        string part = s.substr(0 , i+1);
-        if(isPalin(part)){
+    for(int i=0 ; i<s.size() ; i++){
+        string part = s.substr(0,i+1);
+        if(isValid(part)){
             partition.push_back(part);
             getParts(s.substr(i+1) , partition , ans);
             partition.pop_back();
         }
     }
 }
+
     vector<vector<string>> partition(string s) {
         vector<vector<string>> ans;
         vector<string> partition;
