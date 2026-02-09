f240894c901ab54b8a55b2400a1cad8bbb41e5da
ayush19bisht <ayushbisht415@gmail.com>
2026-02-09 20:22:57 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (53.85%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
 bool isValid(char ch){
-    if(('A'<=ch && ch<='Z') || ('a'<=ch && ch<='z') || ('0'<=ch && ch<='9')){
+    if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
         return true;
     }
-    return false;  
+    return false;
 }
     bool isPalindrome(string s) {
-      int st = 0;
-      int end = s.size()-1;  
-     while(st<end){
-        while(st<end && !isValid(s[st])){
+        int st = 0;
+        int end = s.size()-1;
+        while(st<end){
+            while(st<end && !isValid(s[st])){
+                st++;
+            }
+            while(st<end && !isValid(s[end])){
+                end--;
+            }
+            if(tolower(s[st]) != tolower(s[end])){
+                return false;
+            }
             st++;
-            }
-        while(st<end && !isValid(s[end])){
             end--;
-            }
-        if(tolower(s[st]) != tolower(s[end])){
-            return false;
         }
-            st++;
-            end--;
-        
-     }
-     return true;
+        return true;
     }
 };
