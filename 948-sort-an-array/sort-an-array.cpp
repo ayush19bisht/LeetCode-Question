1090765c96fa658b5d25df13f702482bd59a157c
ayush19bisht <ayushbisht415@gmail.com>
2026-02-11 15:30:48 +0530

    Time: 637 ms (24.26%) | Memory: 295.6 MB (24.30%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-void merge(vector<int>&arr , int st , int mid , int end){
+void merge(vector<int>&arr , int st ,int mid , int end){
     vector<int> temp;
-    int i=st , j=mid+1;
+    int i = st ;
+    int j=mid+1;
     while(i<=mid && j<=end){
-        if(arr[i] <= arr[j]){
+        if(arr[i]<=arr[j]){
             temp.push_back(arr[i]);
             i++;
         }
@@ -21,22 +22,22 @@ void merge(vector<int>&arr , int st , int mid , int end){
         temp.push_back(arr[j]);
             j++;
     }
-    for(int i=0 ; i<temp.size() ; i++){
+    for(int i =0 ;i<temp.size() ; i++){
         arr[i+st] = temp[i];
     }
 }
 void mergeSort(vector<int>&arr , int st , int end){
     if(st<end){
         int mid = st+(end-st)/2;
-        mergeSort(arr , st , mid); //left half
-        mergeSort(arr ,mid+1 , end);   //right half
-        merge(arr , st , mid , end);
+        mergeSort(arr , st , mid);   //left half
+        mergeSort(arr , mid+1 , end);    //right half
+        merge(arr , st ,mid , end);
     }
 }
     vector<int> sortArray(vector<int>& arr) {
-        int st = 0;
+        int st = 0 ;
         int end = arr.size()-1;
-        mergeSort(arr , st ,end);
+        mergeSort(arr , st , end);
         return arr;
     }
 };
