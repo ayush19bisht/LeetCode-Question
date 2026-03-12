bafad188e1bddbddb9e18604168f3fb988b18b18
ayush19bisht <ayushbisht415@gmail.com>
2026-03-12 20:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 31.9 MB (20.52%) - LeetSync

--- 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     bool isValid(vector<vector<int>>& grid , int r , int c , int n , int expVal){
-        if(r<0 || c<0 || r>=n || c>=n || grid[r][c]!=expVal){
+        if(r<0 || c<0 || r>=n || c>=n || grid[r][c] != expVal){
             return false;
         }
         if(expVal == n*n-1){
             return true;
         }
-        // 8 possible moves
         int ans1 = isValid(grid , r-2 , c-1 , n , expVal+1);
         int ans2 = isValid(grid , r-1 , c-2 , n , expVal+1);
         int ans3 = isValid(grid , r+1 , c-2 , n , expVal+1);
@@ -16,10 +15,9 @@ public:
         int ans6 = isValid(grid , r+1 , c+2 , n , expVal+1);
         int ans7 = isValid(grid , r-1 , c+2 , n , expVal+1);
         int ans8 = isValid(grid , r-2 , c+1 , n , expVal+1);
-
-        return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 || ans7 || ans8 ;
+        return ans1 || ans2 || ans3 || ans4 || ans5 || ans6 ||ans7 || ans8;
     }
     bool checkValidGrid(vector<vector<int>>& grid) {
-        return isValid(grid , 0 , 0 , grid.size() , 0);
+        return isValid(grid , 0 ,0 , grid.size() , 0);
     }
 };
